c698f15bedc717152e3711de56d73861e981b398
Arsen <ozhetov.arsen@gmail.com>
2024-02-25 18:16:33 +0600

    fix: change method

--- app/controller.py
+++ app/controller.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
@@ -21,7 +23,7 @@ def get_courier_location(db: Session, courier_id: int):
 def change_courier_location(db: Session, courier_id: int, new_location: dict):
     courier = db.query(Courier).filter(Courier.id == courier_id).first()
     if courier:
-        courier.location = new_location
+        courier.location = json.dumps(new_location)
         db.commit()
         return {"message": "Location updated successfully"}
     raise HTTPException(status_code=404, detail="Courier not found")
